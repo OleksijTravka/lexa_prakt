6d02b628c9144ac537a7dddfdf989cb3e9a6181c
Minikit <44140060+TheMinikit@users.noreply.github.com>
2024-07-22 11:29:02 +0300

    Fix Leha's formula

--- matan.py
+++ matan.py
@@ -1,15 +1,11 @@
 import math
 
-
-
-a  = input()
-b  = input()
-c  = input()
+a = input()
+b = input()
+c = input()
 a = int(a)
 b = int(b)
 c = int(c)
 
 p = pow(b, 2) - (a * c)
-s = b + math.sqrt(p) / ( 2 * a)
-
-print(s)
+s = (b + math.sqrt(p)) / (2 * a)
