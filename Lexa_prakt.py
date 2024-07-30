1ba405e6d3629ce3e97b65eaeb90992759da0c7d
zvery <zveryuza.98@gmail.com>
2024-07-30 18:07:48 +0300

    Вот как то закинул) там пофикси фигню и обясни че)

--- Lexa_prakt.py
+++ Lexa_prakt.py
@@ -1,15 +1,4 @@
-import math
-
-
-
-a  = input()
-b  = input()
-c  = input()
+a = input()
 a = int(a)
-b = int(b)
-c = int(c)
-
-p = pow(b, 2) - (a * c)
-s = b + math.sqrt(p) / ( 2 * a)
-
-print(s)
+b = (a * 13) + (25 * 45) - 7
+print(b)
