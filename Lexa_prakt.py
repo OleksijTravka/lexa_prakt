4f1491650addc47690749bdbb39743f0028b7748
Minikit <44140060+TheMinikit@users.noreply.github.com>
2024-08-01 23:29:34 +0300

    fix digit extraction formula

--- Lexa_prakt.py
+++ Lexa_prakt.py
@@ -1,8 +1,13 @@
+import math
+
 a = input()
 a = int(a)
 
 first = a % 10
-second = round(a / 10) % 10
-third = round(a / 100) % 10
+second = math.floor(a / 10) % 10
+third = math.floor(a / 100) % 10
 
+print(first)
+print(second)
+print(third)
 print(first + second + third)
