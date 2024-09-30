4eac35109867247eb699c2bc6aa90dd7b97f47f5
Lukas <lukas.kirsa@gmail.com>
2024-09-30 19:26:47 +0300

    fixed changes 7.12 pudge invincible

--- Lexa_prakt.py
+++ Lexa_prakt.py
@@ -1,9 +1,10 @@
-a = int(input("Введите номер"))
-if a < 10:
-    print('менше')
-elif a == 10:
-    print("Равно")
-else:
-    print("Больше")
+# a = int(input("Введите номер"))
+# if a < 10:
+#     print('менше')
+# elif a == 10:
+#     print("Равно")
+# else:
+#     print("Больше")
 
-
+for i in range(1,11):
+    print(i)
